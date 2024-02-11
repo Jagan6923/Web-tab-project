a66faf6d1014db83257f5d6b6b3a00b421a8b66a
Jeyaraman S <137631905+Jagan6923@users.noreply.github.com>
2024-02-11 12:01:19 +0530

    validation added

--- formapp/urls.py
+++ formapp/urls.py
@@ -1,8 +1,9 @@
-# urls.py in formapp app
+
 from django.urls import path
-from .views import index, save_project  # Make sure to import the index view
+from .views import index, save_project, validate_project_name
 
 urlpatterns = [
-    path('', index, name='index'),  # Ensure the index view is included in urlpatterns
+    path('', index, name='index'),
     path('save_project/', save_project, name='save_project'),
+    path('validate_project_name/', validate_project_name, name='validate_project_name'),
 ]
--- formapp/views.py
+++ formapp/views.py
@@ -15,3 +15,10 @@ def save_project(request):
             Project.objects.create(name=project_name)
             return JsonResponse({'message': 'Project name saved successfully'})
     return JsonResponse({'message': 'Invalid request'}, status=400)
+
+def validate_project_name(request):
+    if request.method == 'POST':
+        project_name = request.POST.get('project_name')
+        exists = Project.objects.filter(name=project_name).exists()
+        return JsonResponse({'exists': exists})
+    return JsonResponse({'exists': False})
