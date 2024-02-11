40fe448e96f7a48b68ff203bf98fd6eb59a3e678
Jeyaraman S <137631905+Jagan6923@users.noreply.github.com>
2024-02-11 11:18:27 +0530

    database connected

--- formapp/models.py
+++ formapp/models.py
@@ -1,20 +1,5 @@
+# models.py
 from django.db import models
 
-class Form(models.Model):
-    form_id = models.CharField(max_length=100, unique=True)
-    form_input_field_data = models.TextField(default="")
-    redirect_path_input_field = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.form_id
-
-class Field(models.Model):
-    form = models.ForeignKey(Form, related_name='fields', on_delete=models.CASCADE)
-    field_id = models.CharField(max_length=100, unique=True)
-    field_input_data = models.TextField()
-    selection_box_data = models.TextField()
-    is_required = models.BooleanField(default=False)
-    is_checked = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.field_id
+class Project(models.Model):
+    name = models.CharField(max_length=255, unique=True)
--- formapp/urls.py
+++ formapp/urls.py
@@ -1,7 +1,8 @@
+# urls.py in formapp app
 from django.urls import path
-from . import views
+from .views import index, save_project  # Make sure to import the index view
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('submit/', views.submit_form, name='submit_form'),
+    path('', index, name='index'),  # Ensure the index view is included in urlpatterns
+    path('save_project/', save_project, name='save_project'),
 ]
