cc60591f6e35e8353b7a4145bf9ebd3452e70f61
Jeyaraman S <137631905+Jagan6923@users.noreply.github.com>
2024-02-15 22:12:20 +0530

    DB working properly

--- formapp/urls.py
+++ formapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, save_project, validate_project_name
+from .views import index, save_project, validate_project_name, save_draggable_input
 
 urlpatterns = [
     path('', index, name='index'),
     path('save_project/', save_project, name='save_project'),
     path('validate_project_name/', validate_project_name, name='validate_project_name'),
+    path('save_draggable_input/', save_draggable_input, name='save_draggable_input'),
 ]
