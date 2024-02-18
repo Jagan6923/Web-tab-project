796bc07b8d76c637ba6f7e1a9595549ceb2f03d0
Jeyaraman S <137631905+Jagan6923@users.noreply.github.com>
2024-02-18 11:48:15 +0530

    checking

--- formapp/models.py
+++ formapp/models.py
@@ -5,6 +5,6 @@ class Project(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
 class DraggableInput(models.Model):
-    data = models.CharField(max_length=255)  # Add this line
+    data = models.CharField(max_length=255) 
     x_axis = models.FloatField()
     y_axis = models.FloatField()
